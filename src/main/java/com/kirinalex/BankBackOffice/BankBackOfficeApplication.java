f83696a8f29b58a9a9726cc213d1a8ef7c25e212
Alexey <kirinalex.dev@yandex.ru>
2022-11-01 13:35:16 +0700

    init

---
+++ src/main/java/com/kirinalex/BankBackOffice/BankBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.kirinalex.BankBackOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBackOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirinalex/BankBackOffice/BankBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirinalex.BankBackOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
